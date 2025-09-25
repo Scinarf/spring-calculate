81670dbe25d791dc707765ef2fa2d25b4f0b5e33
Scinarf <franciswilliamsoncorp@gmail.com>
2025-09-25 20:06:33 +0200

    I added the other primary functionalities of a calculator

--- src/main/java/com/example/demo/controller/CalculateController.java
+++ src/main/java/com/example/demo/controller/CalculateController.java
@@ -4,10 +4,7 @@ import com.example.demo.dto.CalculateRequest;
 import com.example.demo.entity.CalculateEntity;
 import com.example.demo.service.CalculatorServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.Scanner;
@@ -22,8 +19,23 @@ public class CalculateController {
         this.calculatorService = calculatorService;
     }
 
-    @PostMapping("/add")
+    @GetMapping("/add")
     public int addValues(@RequestBody CalculateRequest request) {
         return calculatorService.add(request.getA(), request.getB());
+    }
+
+    @GetMapping("/minus")
+    public int subtractValues(@RequestBody CalculateRequest request) {
+        return calculatorService.minus(request.getA(), request.getB());
+    }
+
+    @GetMapping("/multiply")
+    public int multiplyValues(@RequestBody CalculateRequest request) {
+        return calculatorService.multiply(request.getA(), request.getB());
+    }
+
+    @GetMapping("/divide")
+    public int divideValues(@RequestBody CalculateRequest request) {
+        return calculatorService.divide(request.getA(), request.getB());
     }
 }
